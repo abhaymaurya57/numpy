f231cddef5c8784ecd103349e8df8e437f22996f
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-09-08 22:42:57 +0530

    rounding

---
+++ 32_rounding_decimals.py
@@ -0,0 +1,9 @@
+# rounding decimal : there are 5 way of rounding
+#  off the decimal in numpy : trancation,fix,rounding,floor,ceil
+
+#truncation: trunc() and fix()
+
+# here we are truncating the below array
+import numpy as np
+a = np.trunc([-3.1666,3.6667])
+print(a)
